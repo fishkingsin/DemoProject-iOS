29dce3c08e81a1f102ecf9996662427b45420f8a
James Kong <fishkingsin@gmail.com>
2018-11-25 18:36:00 +0800

    Initial Commit

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by James Kong on 25/11/2018.
+//  Copyright © 2018 James Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
